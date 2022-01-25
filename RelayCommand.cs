cb77812328d007229e47e5feff65a6c4cb71606b
khilesh55 <61207468+khilesh55@users.noreply.github.com>
2022-01-25 15:31:32 -0700

    Initial port over

---
+++ RelayCommand.cs
@@ -0,0 +1,33 @@
+﻿using Prism.Commands;
+using System;
+using System.Windows.Input;
+
+namespace SpasticityClient
+{
+    public static class ApplicationCommands
+    {
+        public static CompositeCommand ReadCommand = new CompositeCommand();
+    }
+
+    public class RelayCommand : ICommand
+    {
+        private Action _action;
+
+        public RelayCommand(Action action)
+        {
+            _action = action;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _action();
+        }
+
+        public event EventHandler CanExecuteChanged { add { } remove { } }
+    }
+}
